b7e172abefd3629e42347dcc53112c33ad8f411c
pugmarx <naqi.rizvi@gmail.com>
2017-05-11 15:04:40 +0530

    all-to-all version. needs further optimization for Gossip/SWIM.

--- mp1/MP1Node.cpp
+++ mp1/MP1Node.cpp
@@ -109,6 +109,7 @@ int MP1Node::initThisNode(Address *joinaddr) {
 	memberNode->timeOutCounter = -1;
     initMemberListTable(memberNode);
 
+    addToMemberList(id, port, memberNode->heartbeat);
     return 0;
 }
 
@@ -145,9 +146,9 @@ int MP1Node::introduceSelfToGroup(Address *joinaddr) {
 #endif
 
         // send JOINREQ message to introducer member
-        emulNet->ENsend(&memberNode->addr, joinaddr, (char *)msg, msgsize);
+        emulNet->ENsend(&memberNode->addr, joinaddr, (char *)msg, (int)msgsize);
 
-        free(msg);
+        //free(msg);
     }
 
     return 1;
@@ -160,10 +161,9 @@ int MP1Node::introduceSelfToGroup(Address *joinaddr) {
  * DESCRIPTION: Wind up this node and clean up state
  */
 int MP1Node::finishUpThisNode(){
-   #ifdef DEBUGLOG
-        printf("%s \n", "In finishUpThisNode...finishing Up");
-    #endif
-    return 1;
+    memberNode->inGroup = false;
+    memberNode->memberList.clear();
+    return 0;
 }
 
 /**
@@ -199,13 +199,13 @@ void MP1Node::nodeLoop() {
 void MP1Node::checkMessages() {
     void *ptr;
     int size;
-
+    
     // Pop waiting messages from memberNode's mp1q
     while ( !memberNode->mp1q.empty() ) {
-    	ptr = memberNode->mp1q.front().elt;
-    	size = memberNode->mp1q.front().size;
-    	memberNode->mp1q.pop();
-    	recvCallBack((void *)memberNode, (char *)ptr, size);
+        ptr = memberNode->mp1q.front().elt;
+        size = memberNode->mp1q.front().size;
+        memberNode->mp1q.pop();
+        recvCallBack((void *)memberNode, (char *)ptr, size);
     }
     return;
 }
@@ -216,32 +216,118 @@ void MP1Node::checkMessages() {
  * DESCRIPTION: Message handler for different message types
  */
 bool MP1Node::recvCallBack(void *env, char *data, int size ) {
-	#ifdef DEBUGLOG
-        printf("%s \n", "in recvCallBack...will handle different types of msgs here ...");
-    #endif
+#ifdef DEBUGLOG
+    printf("%s \n", "in recvCallBack...will handle different types of msgs here ...");
+#endif
     
     //detect the message type and act on it!
-    
     MessageHdr* msgHeader = (MessageHdr*) data;
-    MsgTypes msgType = msgHeader->msgType;
+    Address originAddress;
+    long originHeartbeat;
+    memcpy(&originAddress, (char *)(msgHeader+1), sizeof(memberNode->addr.addr));
+    memcpy(&originHeartbeat, (char *)(msgHeader+1) + 1 + sizeof(memberNode->addr.addr), sizeof(long));
+    long nMem = memberNode->memberList.size();
+    size_t msgLength = sizeof(MessageHdr) + sizeof(long) + nMem * sizeof(MemberListEntry);
+    int msgType = msgHeader->msgType;
+    
+#ifdef DEBUGLOG
+    printf("%s:%u \n","**** Received msgType ", msgType);
+#endif
+    
     
     switch(msgType){
-    
-        case JOINREQ:
-            #ifdef DEBUGLOG
-                cout << " **** Received JOINREQ " << msgType << endl;
-                //log->LOG(msgType, "***** RECEIVED JOINREQ");
-            #endif
+            
+        case JOINREQ: {
+            
+            int id = *(int*)(&originAddress.addr[0]);
+            int port = *(short*)(&originAddress.addr[4]);
+            
+            addToMemberList(id, port, originHeartbeat);
+            
+            MessageHdr *outgoingMsg = (MessageHdr *) malloc(msgLength * sizeof(char));
+            outgoingMsg->msgType = JOINREP;
+            memcpy((char *)(outgoingMsg+1), &nMem, sizeof(int));
+            char *sz = (char *)(outgoingMsg+1) + sizeof(int);
+            
+            for (vector<MemberListEntry>::iterator it = memberNode->memberList.begin();
+                 it != memberNode->memberList.end(); ++it){
+                memcpy(sz, &(*it), sizeof(MemberListEntry));
+                sz += sizeof(MemberListEntry);
+                
+            }
+            emulNet->ENsend(&memberNode->addr, &originAddress, (char*)outgoingMsg, msgLength);
+            //free(outgoingMsg);
             break;
+        }
+        case JOINREP:{ //must've recvd memberList
+            memberNode->inGroup = true;
+            //break;
+        }
+        case HEARTBEAT:{//recvd memberList
+            char *msgContent = (char *)(msgHeader+1);
+            int n;
+            memcpy(&n, (char *)msgContent, sizeof(int));
+            MemberListEntry* recvdMemberList = (MemberListEntry*) (msgContent + sizeof(int));
+            for (int i = 0; i < n; i++) {
+                
+                int id = recvdMemberList[i].getid();
+                short port = recvdMemberList[i].getport();
+                long hbeat = recvdMemberList[i].getheartbeat();
+                
+                addToMemberList(id, port, hbeat);
+            }
+            break;
+        }
         default:
-            #ifdef DEBUGLOG
-                cout << "**** Received OTHER " << msgType << endl;
-            #endif
+            break;
             
     }
+#ifdef DEBUGLOG
+    printf("Memberlist now is....\n");
+#endif
+    printMemberListSize();
+    return true;
     
-    return false;
 }
+
+
+
+//bool MP1Node::sendMessage(Address *dest, char* msg, int msgLength){
+//    long n = memberNode->memberList.size();
+//    MessageHdr* msgHeader = (MessageHdr*) msg;
+//    Address originAddress;
+//    memcpy(&originAddress, (char *)(msgHeader+1), sizeof(memberNode->addr.addr));
+//    MessageHdr *outgoingMsg = (MessageHdr *) malloc(msgLength * sizeof(char));
+//    outgoingMsg->msgType = msgType;
+//    memcpy((char *)(outgoingMsg+1), &n, sizeof(int));
+//    char *p = (char *)(outgoingMsg+1) + sizeof(int);
+//    
+//    for (vector<MemberListEntry>::iterator it = memberNode->memberList.begin();
+//         it != memberNode->memberList.end(); ++it){
+//        memcpy(p, &(*it), sizeof(MemberListEntry));
+//        p += sizeof(MemberListEntry);
+//        
+//    }
+//    emulNet->ENsend(&memberNode->addr, &originAddress, (char*)outgoingMsg, (int)msgLength);
+//    free(outgoingMsg);
+//
+//    return true;
+//}
+
+
+void MP1Node::updateMemberListEntry(int id, short port, long heartbeat, long ts){
+    
+    for (vector<MemberListEntry>::iterator it = memberNode->memberList.begin();
+         it != memberNode->memberList.end(); ++it){
+        if(it->id == id && it->port==port && it->heartbeat < heartbeat){
+        //if(it->id == id && it->heartbeat < heartbeat){
+            it->heartbeat = heartbeat;
+            it->timestamp = par->getcurrtime();
+        }
+    }
+    
+}
+
 
 /**
  * FUNCTION NAME: nodeLoopOps
@@ -251,13 +337,93 @@ bool MP1Node::recvCallBack(void *env, char *data, int size ) {
  * 				Propagate your membership list
  */
 void MP1Node::nodeLoopOps() {
-
-	#ifdef DEBUGLOG
-        printf("%s \n", "in nodeLoopOps... will remove nodes that seem dead...");
-    #endif
-
+    
+#ifdef DEBUGLOG
+    printf("%s:%lu \n", ">>>> BEFORE cleanup of dead nodes..size", memberNode->memberList.size());
+#endif
+    
+    // Iterate the list and remove members that haven't responded within the TREMOVE units
+    // int timeOut = memberNode -> timeOutCounter;
+    long size = memberNode->memberList.size();
+    for (vector<MemberListEntry>::iterator it = memberNode->memberList.begin();
+         it != memberNode->memberList.end() && size>0; ) {
+        
+        long delta = par->getcurrtime() - it->timestamp;
+        //        if ( delta >= TFAIL && delta < TREMOVE) { // seems failed!
+        //            Address dest = getAddress(it->getid(), it->getport());
+        //            //memberNode->memberList.erase(it);
+        //            //log->logNodeRemove(&(memberNode->addr), &dest);
+        //        }
+        
+#ifdef DEBUGLOG
+        printf("%s %ld\n", "** delta is", delta);
+#endif
+        if (delta >= TREMOVE ) {
+            Address dest = getAddress(it->getid(), it->getport());
+            
+            if(!isMyAddress(&dest)){
+                it = memberNode->memberList.erase(it);
+                size--;
+                log->logNodeRemove(&(memberNode->addr), &dest);
+            }
+        } else {
+            it++;
+        }
+    }
+    
+    
+#ifdef DEBUGLOG
+    printf("%s:%lu \n", ">>>> AFTER cleanup of dead nodes..size", memberNode->memberList.size());
+    printMemberListSize();
+#endif
+    
+    // increment my h/b
+    memberNode->heartbeat++;
+    long nMem = memberNode->memberList.size();
+    size_t msgLength = sizeof(MessageHdr) + sizeof(long) + nMem * sizeof(MemberListEntry);
+    MessageHdr *outgoingMsg = (MessageHdr *) malloc(msgLength * sizeof(char));
+    outgoingMsg->msgType = HEARTBEAT;
+    memcpy((char *)(outgoingMsg+1), &nMem, sizeof(int));
+    char *sz = (char *)(outgoingMsg+1) + sizeof(int);
+    for (vector<MemberListEntry>::iterator it = memberNode->memberList.begin();
+         it != memberNode->memberList.end(); ++it){
+        memcpy(sz, &(*it), sizeof(MemberListEntry));
+        sz += sizeof(MemberListEntry);
+        
+    }
+    
+    //send to all members (can be optimized to send to a random set!!)
+    for (vector<MemberListEntry>::iterator it = memberNode->memberList.begin();
+         it != memberNode->memberList.end(); ++it) {
+        
+        Address destAddr = getAddress(it->id, it->port);
+        
+        if(isMyAddress(&destAddr)){
+            //continue;
+            it->heartbeat=memberNode->heartbeat;
+            it->timestamp=par->getcurrtime();
+            continue;
+        }
+        //---
+        emulNet->ENsend(&memberNode->addr, &destAddr, (char*)outgoingMsg, msgLength);
+        //---
+        
+    }
+    //free(outgoingMsg);
     return;
 }
+
+Address MP1Node::getAddress(int id, short port){
+    Address addr;
+    
+    memset(&addr, 0, sizeof(Address));
+    memcpy(&addr.addr[0], &id, sizeof(int));
+    memcpy(&addr.addr[4], &port, sizeof(short));
+    
+    return addr;
+
+}
+
 
 /**
  * FUNCTION NAME: isNullAddress
@@ -292,9 +458,85 @@ void MP1Node::initMemberListTable(Member *memberNode) {
 	memberNode->memberList.clear();
 }
 
-void MP1Node::addToMemberList(int id, short port, Member *memberNode){
-    MemberListEntry memListEntry = MemberListEntry(id, port, par->getcurrtime(), memberNode->heartbeat);
-    memberNode->memberList.push_back(memListEntry);
+void MP1Node::addToMemberList(int id, short port, long heartbeat){
+    Address addr = getAddress(id, port);
+    
+    #ifdef DEBUGLOG
+        printf("####Checking if id [%d:%d] already exists in memberList...\n",id, port);
+        printMemberListSize();
+    #endif
+    
+    if(!existsInMemberList(id, port)){
+#ifdef DEBUGLOG
+        printf("### NO it DOES NOT..adding as a new member\n");
+#endif
+        
+        long ts = par->getcurrtime();
+#ifdef DEBUGLOG
+        printf("### TS is:[%lu]\n",ts);
+#endif
+        MemberListEntry mle = MemberListEntry(id, port, heartbeat, ts);
+        memberNode->memberList.push_back(mle);
+
+#ifdef DEBUGLOG
+        log->logNodeAdd(&memberNode->addr, &addr);
+#endif
+        //free(&mle);
+        //delete mle;
+        
+        
+    }else{ // update
+#ifdef DEBUGLOG
+        printf("### YES it DOES...will update hb and ts if necessary\n");
+#endif
+        updateMemberListEntry(id, port, heartbeat, par->getcurrtime());
+        
+    }
+    
+#ifdef DEBUGLOG
+    //printf("#### Status after add/update ....\n");
+    //printMemberListSize();
+#endif
+    
+}
+
+bool MP1Node::isMyAddress(Address *addr){
+    return (memcmp((char*)&(memberNode->addr.addr), (char*)&(addr->addr), sizeof(memberNode->addr.addr)) == 0);
+}
+
+bool MP1Node::existsInMemberList(int id, short port){
+    bool exists = false;
+    for(std::vector<MemberListEntry>::iterator it = memberNode->memberList.begin();
+        it != memberNode->memberList.end(); ++it) {
+        if(it->id == id && it->port==port) {
+        //if(it->id == id) {
+            exists = true;
+            break;
+        }
+    }
+    return exists;
+}
+
+
+
+void MP1Node::printMemberListSize(){
+    
+#ifdef DEBUGLOG
+    printf("=========== At NodeId:[%d] =============\n", memberNode->addr.addr[0]);
+#endif
+    for(std::vector<MemberListEntry>::iterator it = memberNode->memberList.begin();
+        it != memberNode->memberList.end(); ++it) {
+#ifdef DEBUGLOG
+        printf("[%d:%d:%ld:%ld]", it->id,it->port,it->heartbeat,it->timestamp);
+#endif
+    }
+#ifdef DEBUGLOG
+    printf("\n______________________________\n");
+#endif
+    
+//#ifdef DEBUGLOG
+//    printf("**** Memberlist count is %lu\n", memberNode -> memberList.size());
+//#endif
 }
 
 /**
